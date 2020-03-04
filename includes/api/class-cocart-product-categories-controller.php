7320e0390dd9c00bd055c80f574b93a5f41fd005
Sébastien Dumont <mailme@sebastiendumont.com>
2020-03-04 19:52:17 +0100

    Removed context request

--- includes/api/class-cocart-product-categories-controller.php
+++ includes/api/class-cocart-product-categories-controller.php
@@ -80,9 +80,8 @@ class CoCart_Product_Categories_Controller extends CoCart_REST_Terms_Controller 
 			);
 		}
 
-		$context = ! empty( $request['context'] ) ? $request['context'] : 'view';
 		$data    = $this->add_additional_fields_to_object( $data, $request );
-		$data    = $this->filter_response_by_context( $data, $context );
+		$data    = $this->filter_response_by_context( $data, 'view' );
 
 		$response = rest_ensure_response( $data );
 
