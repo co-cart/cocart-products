5bff0957bf9fef333c47f1698e56fe1f1137f8cf
Sébastien Dumont <mailme@sebastiendumont.com>
2020-03-30 20:25:42 +0200

    Bumped WC requirement and tested

--- cocart-products.php
+++ cocart-products.php
@@ -9,8 +9,8 @@
  * Text Domain: cocart-products
  * Domain Path: /languages/
  *
- * WC requires at least: 3.0.0
- * WC tested up to: 3.9.3
+ * WC requires at least: 3.6.0
+ * WC tested up to: 4.0.1
  *
  * Copyright: © 2020 Sébastien Dumont, (mailme@sebastiendumont.com)
  *
